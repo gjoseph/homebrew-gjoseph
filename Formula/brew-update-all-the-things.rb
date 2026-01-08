a8dab49df74b8875885d8b4f418dc6242d68ddba
Gregory Joseph <greg.jos@gmail.com>
2026-01-08 13:09:01 +1100

    Testing 0.0.1

--- Formula/brew-update-all-the-things.rb
+++ Formula/brew-update-all-the-things.rb
@@ -1,8 +1,8 @@
 class BrewUpdateAllTheThings < Formula
   desc "Updates all Brew formulae and casks on a schedule"
   homepage "https://github.com/gjoseph/brew-update-all-the-things"
-  url "https://github.com/gjoseph/brew-update-all-the-things/archive/v0.0.1.tar.gz"
-  sha256 "abc123..." # SHA256 of the tarball
+  url "https://github.com/gjoseph/brew-update-all-the-things/releases/download/v0.0.1/brew-update-all-the-things"
+  sha256 "ba2838c6721366595d61420744151e1f5e4c580c7b11b0791a3c16e445b71599"
   license "GNU GPLv3"
 
   def install
